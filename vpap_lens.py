0e98ab4c951df330bb5c0a83ea8be9662f8131b5
eagleEggs <29800532+eagleEggs@users.noreply.github.com>
2019-09-14 18:58:27 -0400

    loops through zips, and pages until complete.
    Needs output formatting of final list (remove spaces / newlines (\n).

--- vpap_lens.py
+++ vpap_lens.py
@@ -1,49 +1,76 @@
 import requests
 from lxml import html
+from time import sleep
 
-# build zip code list (to loop through and build URL dynamically):
-zips = [22201, 22202]  # TODO we need to get all of the zip codes here
+# https://www.vpap.org/sitemap-donors.xml
+# https://www.vpap.org/sitemap-candidates.xml
 
-start_year = "2018"   # TODO make this an input to choose years
+zips = [22201]
+
+start_year = "2018"
 end_year = "2019"
 
 url_list = []
 results = []
+final_list = []
 
 for zip in zips:
-    
-    # build the URL (with xip and dates modified:
     url = "https://www.vpap.org/" \
     "zipcodes/zcta/{}/donors/" \
     "?page=all" \
-    "&zip=22204" \
+    "&zip={}" \
     "&start_year={}" \
     "&end_year={}" \
-    "&recip_type=all".format(zip, start_year, end_year)
-    
-    # append url to list of urls (for each zip code):
+    "&recip_type=all".format(zip, zip, start_year, end_year)
+    print(url)
     url_list.append(url)
-    
-    # get html from url:
     page = requests.get(url)
-    
-    # convert content for lxml:
     tree = html.fromstring(page.content)
-    
-    # grab the xpath of the the donor and price tables:
     prices = tree.xpath('//td[@class="right"]/text()')
     donors = tree.xpath('//tr/td/a/text()')
 
-    final_list = []
     i = 0
-    
-    # loop through first list and match to second:
     for price in prices:
-        final_list.append("{}, {}".format(price, donors[i]))
+        final_list.append(["{}, {}".format(price, donors[i])])
         i = i + 1
 
+    total_pages = tree.xpath('//*/a[@class="page-link page"]/text()')
+    # print(total_pages[2])
+
+
+    for paged in range(1, int(total_pages[2]) + 1):
+        sleep(3)
+        url = "https://www.vpap.org/" \
+              "zipcodes/zcta/{}/donors/" \
+              "?page={}" \
+              "&zip={}" \
+              "&start_year={}" \
+              "&end_year={}" \
+              "&recip_type=all".format(zip, paged, zip, start_year, end_year)
+
+        print(url)
+        url_list.append(url)
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        prices = tree.xpath('//td[@class="right"]/text()')
+        donors = tree.xpath('//tr/td/a/text()')
+
+        i = 0
+        for price in prices:
+            final_list.append(["{}, {}".format(price, donors[i])])
+            i = i + 1
+
+        print(final_list)
+        with open("zip_{}_page_{}.txt".format(zip, paged), 'a') as file:
+            for item in final_list:
+                file.write(item[0])
 
 print(final_list)
+with open("final_output.txt", 'w') as file:
+    for item in final_list:
+        file.write(item[0])
+
+
 
 
 
