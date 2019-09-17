cc5b36d12e3c2656c5b868ca2c456a5a0d8cccf3
eagleEggs <29800532+eagleEggs@users.noreply.github.com>
2019-09-17 17:18:39 -0400

    added comments through code

--- vpap_lens.py
+++ vpap_lens.py
@@ -7,19 +7,24 @@ import random
 # https://www.vpap.org/sitemap-donors.xml
 # https://www.vpap.org/sitemap-candidates.xml
 
+# enter desired zipcodes into this list to iterate through:
 zips = [22201, 22202, 22203]
 
+# enter the start and end year for the term which you'd like to parse:
 start_year = "2018"
 end_year = "2019"
 
-url_list = []
-results = []
-final_list = []
-df_price_list = []
-df_donor_list = []
-df_zip_list = []
+# instantiating various lists to use throughout:
+url_list = [] # track urls parsed
+results = [] # unused should remove
+final_list = [] # final list of prices / donors
+df_price_list = [] # price list for pandas df
+df_donor_list = [] # donor list for pandas df
+df_zip_list = [] # zipcode list for pandas df
 
+# loop through zip code list:
 for zip in zips:
+    # set up URL for parsing, replacing variables as appropriate:
     url = "https://www.vpap.org/" \
     "zipcodes/zcta/{}/donors/" \
     "?page=all" \
@@ -28,13 +33,16 @@ for zip in zips:
     "&end_year={}" \
     "&recip_type=all".format(zip, zip, start_year, end_year)
     print(url)
-    url_list.append(url)
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
+    url_list.append(url) # save URL in list
+    page = requests.get(url) # make call to url and save HTML
+    tree = html.fromstring(page.content) # lxml call to reformat HTML
+    # parse LXML to get prices (using XPATH on site HTML)
     prices = tree.xpath('//td[@class="right"]/text()[normalize-space()]')
+    # parse LXML to get donors (using XPATH on site HTML)
     donors = tree.xpath('//tr/td/a/text()[normalize-space()]')
 
-    i = 0
+    i = 0 # setting iterator to match price list to donor list
+    # loop through prices, and append new df list, and adjacent donor and zip code df list evenly (using i)
     for price in prices:
         price = price.strip()
         donor = donors[i].strip()
@@ -43,12 +51,15 @@ for zip in zips:
         df_zip_list.append(zip)
         final_list.append(["{}, {}".format(price, donor, zip)])
         i = i + 1
-
+    
+    # grab the last page possible from LXML object (HTML) using XATH from page
     total_pages = tree.xpath('//*/a[@class="page-link page"]/text()')
-    # print(total_pages[2])
 
+    # loop through all pages, since you did page 1 already, start on 2 and go to the total_pages amount
     for paged in range(2, int(total_pages[2]) + 1):
-        sleep(random.randrange(1, 10))
+        sleep(random.randrange(1, 10)) # sleep for a random interval of 1 to 10 seconds
+        
+        # build new URL
         url = "https://www.vpap.org/" \
               "zipcodes/zcta/{}/donors/" \
               "?page={}" \
@@ -76,16 +87,15 @@ for zip in zips:
 
         print(final_list)
 
-        # with open("zip_{}_page_{}.txt".format(zip, paged), 'a') as file:
-        #     for item in final_list:
-        #        file.write(item[0])
-
+# build dataframe from all data accumulated (three columns - labeled)
 df = DataFrame({'Donor Name': df_donor_list,
                 'Donation Amount': df_price_list,
                 'Zip Code': df_zip_list})
 
 print(final_list)
 print(df)
+
+# save df to csv format
 df.to_csv('final_output_csv.txt', sep='\t', index=False)
 
 
