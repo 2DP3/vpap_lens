91f33cd35cca0c0b3d1100de89129d87efa1588c
eagleEggs <29800532+eagleEggs@users.noreply.github.com>
2019-09-16 21:46:55 -0400

    pipe to dataframe>output tab delimited txt (excel)
    When opening in excel, data is TAB delimited

--- vpap_lens.py
+++ vpap_lens.py
@@ -1,11 +1,13 @@
 import requests
 from lxml import html
 from time import sleep
+from pandas import DataFrame
+import random
 
 # https://www.vpap.org/sitemap-donors.xml
 # https://www.vpap.org/sitemap-candidates.xml
 
-zips = [22201]
+zips = [22201, 22202, 22203]
 
 start_year = "2018"
 end_year = "2019"
@@ -13,6 +15,9 @@ end_year = "2019"
 url_list = []
 results = []
 final_list = []
+df_price_list = []
+df_donor_list = []
+df_zip_list = []
 
 for zip in zips:
     url = "https://www.vpap.org/" \
@@ -26,20 +31,24 @@ for zip in zips:
     url_list.append(url)
     page = requests.get(url)
     tree = html.fromstring(page.content)
-    prices = tree.xpath('//td[@class="right"]/text()')
-    donors = tree.xpath('//tr/td/a/text()')
+    prices = tree.xpath('//td[@class="right"]/text()[normalize-space()]')
+    donors = tree.xpath('//tr/td/a/text()[normalize-space()]')
 
     i = 0
     for price in prices:
-        final_list.append(["{}, {}".format(price, donors[i])])
+        price = price.strip()
+        donor = donors[i].strip()
+        df_price_list.append(price.strip())
+        df_donor_list.append(donor.strip())
+        df_zip_list.append(zip)
+        final_list.append(["{}, {}".format(price, donor, zip)])
         i = i + 1
 
     total_pages = tree.xpath('//*/a[@class="page-link page"]/text()')
     # print(total_pages[2])
 
-
     for paged in range(1, int(total_pages[2]) + 1):
-        sleep(3)
+        sleep(random.randrange(1, 10))
         url = "https://www.vpap.org/" \
               "zipcodes/zcta/{}/donors/" \
               "?page={}" \
@@ -52,23 +61,32 @@ for zip in zips:
         url_list.append(url)
         page = requests.get(url)
         tree = html.fromstring(page.content)
-        prices = tree.xpath('//td[@class="right"]/text()')
-        donors = tree.xpath('//tr/td/a/text()')
+        prices = tree.xpath('//td[@class="right"]/text()[normalize-space()]')
+        donors = tree.xpath('//tr/td/a/text()[normalize-space()]')
 
         i = 0
         for price in prices:
-            final_list.append(["{}, {}".format(price, donors[i])])
+            price = price.strip()
+            donor = donors[i].strip()
+            df_price_list.append(price)
+            df_donor_list.append(donor)
+            df_zip_list.append(zip)
+            final_list.append(["{}, {}".format(price, donor, zip)])
             i = i + 1
 
         print(final_list)
-        with open("zip_{}_page_{}.txt".format(zip, paged), 'a') as file:
-            for item in final_list:
-                file.write(item[0])
+
+        # with open("zip_{}_page_{}.txt".format(zip, paged), 'a') as file:
+        #     for item in final_list:
+        #        file.write(item[0])
+
+df = DataFrame({'Donor Name': df_donor_list,
+                'Donation Amount': df_price_list,
+                'Zip Code': df_zip_list})
 
 print(final_list)
-with open("final_output.txt", 'w') as file:
-    for item in final_list:
-        file.write(item[0])
+print(df)
+df.to_csv('final_output_csv.txt', sep='\t', index=False)
 
 
 
